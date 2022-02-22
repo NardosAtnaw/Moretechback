1e0d4f85aedcd3f7b73f5dc9787264b05d87b854
Nardi <nardidumessa@gmail.com>
2022-02-22 10:09:34 +0300

    move to versavvy

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,7 @@
 <?php
 // $connection = mysqli_connect('localhost','root','','Moretech');
-$connection = mysqli_connect('localhost','moretechplccom_admin ','cptV_&,2B2C.');
+// $connection = mysqli_connect('localhost', 'moretechplccom_admin ', 'cptV_&,2B2C.', 'moretechplccom_backend');
+$connection = mysqli_connect('localhost', 'versavvymediacom_moretechUser ', '=4T(kn1o8ct[', 'versavvymediacom_moretechBack');
 if (!$connection) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
