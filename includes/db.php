09fe0dd05d24b26320865ca59b3be612595fe27f
Nardi <nardidumessa@gmail.com>
2022-02-22 09:32:11 +0300

    db fix

--- includes/db.php
+++ includes/db.php
@@ -1,13 +1,12 @@
 <?php
 // $connection = mysqli_connect('localhost','root','','Moretech');
-$connection = mysqli_connect('localhost','moretechplccom_user ','GrC+-4(g9!a9','moretechplccom_db');
+$connection = mysqli_connect('localhost','moretechplccom_admin ','BBJEk1H@CITt','moretechplccom_backend');
 if (!$connection) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
-
 
 
 
